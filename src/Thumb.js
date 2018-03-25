06083066fa1fff4cdbc8faec175edf75d8991504
JoaoCnh <joaomarinho.cunha@gmail.com>
2018-03-25 15:21:39 +0100

    Accepting multiple faced photos and setting new photos without refresh

--- src/Thumb.js
+++ src/Thumb.js
@@ -9,95 +9,88 @@ const Wrapper = styled.div`
 const Face = styled.div`
   position: absolute;
   border: 1px solid yellow;
+  width: ${props => props.width}px;
+  height: ${props => props.height}px;
+  top: ${props => props.top}px;
+  left: ${props => props.left}px;
 `;
 
 const Eye = styled.div`
+  width: 30%;
+  height: 30%;
   position: absolute;
   border: 1px solid blue;
+  top: calc(${props => props.y - props.top}px - 15%);
+  left: calc(${props => props.x - props.left}px - 15%);
 `;
 
 const Mouth = styled.div`
+  width: 60%;
+  height: 30%;
   position: absolute;
   border: 1px solid orange;
+  top: calc(${props => props.y - props.top}px - 15%);
+  left: calc(${props => props.x - props.left}px - 30%);
 `;
 
 export default class Thumb extends Component {
+  state = {
+    faces: [],
+  };
+
   _imgRef = img => (this.img = img);
-  _faceRef = face => (this.face = face);
-  _mouthRef = mouth => (this.mouth = mouth);
-  _leftEyeRef = leftEye => (this.leftEye = leftEye);
-  _wrapperRef = wrapper => (this.wrapper = wrapper);
-  _rightEyeRef = rightEye => (this.rightEye = rightEye);
 
   _detectFaces = async () => {
     const faceDetector = new window.FaceDetector();
 
     const faces = await faceDetector.detect(this.img);
 
-    faces.forEach(face => {
-      const { top, left, width, height } = face.boundingBox;
-
-      this.face.style.cssText = `
-            width: ${width}px;
-            height: ${height}px;
-            top: ${top}px;
-            left: ${left}px;
-        `;
-
-      face.landmarks.forEach((landmark, index) => {
-        const { x, y } = landmark.location;
-
-        switch (landmark.type) {
-          case "eye":
-            const css = `
-                    width: 35%;
-                    height: 35%;
-                    top: ${y - top}px;
-                    left: ${x - left}px;
-                `;
-
-            if (index === 0) {
-              this.leftEye.style.cssText = css;
-            } else {
-              this.rightEye.style.cssText = css;
-            }
-            break;
-          case "mouth":
-            this.mouth.style.cssText = `
-                    width: 50%;
-                    height: 20%;
-                    top: ${y - top}px;
-                    left: ${x - left}px;
-                `;
-            break;
-        }
-      });
-    });
+    this.setState({ faces });
   };
 
+  componentWillReceiveProps(nextProps) {
+    this.reader.readAsDataURL(nextProps.file);
+  }
+
   componentDidMount() {
-    const reader = new FileReader();
+    this.reader = new FileReader();
 
-    reader.onloadend = () => {
-      this.img.src = reader.result;
-
-      // welp
-      setTimeout(this._detectFaces, 750);
+    this.reader.onloadend = () => {
+      this.img.src = this.reader.result;
     };
 
-    reader.readAsDataURL(this.props.file);
+    this.img.onload = this._detectFaces;
+
+    this.reader.readAsDataURL(this.props.file);
   }
 
   render() {
     const { file, index } = this.props;
+    const { faces } = this.state;
 
     return (
-      <Wrapper innerRef={this._wrapperRef}>
+      <Wrapper>
         <img alt={file.name} ref={this._imgRef} />
-        <Face innerRef={this._faceRef} />
-        <Eye innerRef={this._leftEyeRef} />
-        <Eye innerRef={this._rightEyeRef} />
-        <Mouth innerRef={this._mouthRef} />
+
+        {faces.map((face, faceIndex) => {
+          const { top, left, width, height } = face.boundingBox;
+
+          return (
+            <Face key={`face-${faceIndex}`} top={top} left={left} width={width} height={height}>
+              {face.landmarks.map((landmark, index) => {
+                const { x, y } = landmark.location;
+                const key = `landmark-${faceIndex}-${index}`;
+
+                switch (landmark.type) {
+                  case "eye":
+                    return <Eye key={key} x={x} y={y} top={top} left={left} />;
+                  case "mouth":
+                    return <Mouth key={key} x={x} y={y} top={top} left={left} />
+                }
+              })}
+            </Face>
+          );
+        })}
       </Wrapper>
     );
   }
